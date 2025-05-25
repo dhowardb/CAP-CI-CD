87120e1539a99c919b1770f88b04fea3ca9b0ea0
howard baking <howard.others6@gmail.com>
2025-05-25 15:54:15 +0000

    sample app

---
+++ app/samplebooks/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'samplebooks/test/integration/FirstJourney',
+		'samplebooks/test/integration/pages/BooksList',
+		'samplebooks/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('samplebooks') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
